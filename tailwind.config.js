0fee706fd7ee63da8d3c0438dcb3e7c02da8c4e4
dudz <eduardo_vteixeira@outlook.com>
2024-12-06 18:50:56 -0300

    Initial commit
    - already config tailwind with colors and backgrounds

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{ts,tsx}'],
+	theme: {
+		colors: {
+			blue: {
+				thick: '#5964E0',
+				soft: '#939BF4',
+				veryDark: '#19202D',
+				midnight: '#121721',
+			},
+			pureWhite: '#FFFFFF',
+			gray: {
+				lightGrey: '#F4F6F8',
+				darkGrey: '#6E8098',
+			},
+		},
+		extend: {
+			backgroundImage: {
+				headerMobilePattern: "url('/images/mobile/bg-pattern-header.svg')",
+				headerTabletPattern: "url('/images/tablet/bg-pattern-header.svg')",
+				headerDesktopPattern: "url('/images/desktop/bg-pattern-header.svg')",
+				mobileIconFilter: "url('/images/mobile/icon-filter.svg')",
+				iconCheck: "url('/images/common/icon-check.svg')",
+				iconLocation: "url('/images/common/icon-location.svg')",
+				iconSearch: "url('/images/common/icon-search.svg')",
+				iconMoon: "url('/images/common/icon-moon.svg')",
+				iconSun: "url('/images/common/icon-sun.svg')",
+				logo: "url('/images/common/logo.svg')",
+			},
+
+			fontFamily: {
+				sans: ['Kumbh Sans', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
