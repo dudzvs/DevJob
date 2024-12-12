1af28f58a599a899dec6a3d7293f44aab8cbb96c
dudz <eduardo_vteixeira@outlook.com>
2024-12-12 13:24:09 -0300

    style: responsiveness is boring but i think it's done

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,11 @@
 import { ButtonType } from '../Types.ts';
 
-const Button = ({ optionClasses, type }: ButtonType) => {
-	return <button className={`${optionClasses} rounded bg-center bg-no-repeat`} type={type}></button>;
+const Button = ({ optionClasses, type, children }: ButtonType) => {
+	return (
+		<button className={`${optionClasses} rounded bg-center bg-no-repeat`} type={type}>
+			<span className="hidden sm:block">{children}</span>
+		</button>
+	);
 };
 
 export default Button;
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,14 +5,14 @@ function SearchBar() {
 	const handleInputChange = () => {}; // Probably you will create this funct in the content component, i dnt know we gonna research tomorrow
 
 	return (
-		<div className="absolute left-1/2 right-1/2 top-28 flex h-20 w-[90%] -translate-x-1/2 transform justify-between rounded bg-gray-light p-4 shadow-2xl">
+		<div className="absolute left-1/2 right-1/2 top-28 flex h-20 w-[90%] -translate-x-1/2 transform justify-between rounded bg-gray-light p-4 shadow-2xl lg:max-w-[70%]">
 			<form className="flex w-full items-center justify-between p-2">
 				<Input
 					inputType={'text'}
 					example={'Filter by Title'}
 					inputValue={''}
 					onChange={handleInputChange}
-					optionsClasses={'w-full'}
+					optionsClasses={'w-full sm:bg-icon-search sm:pl-9 pl-2'}
 					name={'title'}
 				/>
 				<Input
@@ -20,26 +20,34 @@ function SearchBar() {
 					inputValue={''}
 					example={'Filter by location'}
 					onChange={handleInputChange}
-					optionsClasses={'w-full hidden sm:block'}
+					optionsClasses={'w-full hidden sm:block bg-icon-location pl-9'}
 					name={'location'}
 				/>
 				<label
 					htmlFor="inputCheckbox"
-					className="mr-4 hidden h-[24px] w-[54px] border-2 border-gray-neutral bg-icon-check sm:block"
+					className="mr-4 hidden min-h-[24px] min-w-[24px] cursor-pointer border border-gray-neutral bg-icon-check bg-center bg-no-repeat sm:block"
 				></label>
 				<Input
 					inputType={'checkbox'}
 					inputValue={''}
-					example={''}
 					onChange={handleInputChange}
 					optionsClasses={'hidden'}
 					id={'inputCheckbox'}
 					name={'isFullTime'}
 				/>
-				<span className="hidden text-nowrap font-bold sm:block">Full Time</span>
+				<span className="hidden text-nowrap text-base font-bold sm:block">Full Time</span>
 				<div className="flex items-center">
-					<Button optionClasses={'bg-icon-filter bg-transparent w-5 h-5 mr-6 ml-6'} type={'button'} />
-					<Button optionClasses={'bg-icon-search-white bg-blue-thick w-12 h-12'} type={'submit'} />
+					<Button
+						optionClasses={'bg-icon-filter bg-transparent w-5 h-5 mr-6 ml-6 sm:hidden'}
+						type={'button'}
+					/>
+					<Button
+						optionClasses={
+							'bg-icon-search-white bg-blue-thick w-12 sm:w-20 h-12 sm:bg-none text-pureWhite font-bold sm:ml-5'
+						}
+						type={'submit'}
+						children={'Search'}
+					/>
 				</div>
 			</form>
 		</div>
@@ -49,7 +57,7 @@ function SearchBar() {
 function Input({ inputType, inputValue, example, onChange, optionsClasses, id }: InputTypes) {
 	return (
 		<input
-			className={`${optionsClasses} outline-none`}
+			className={`${optionsClasses} bg-transparent bg-no-repeat outline-none`}
 			onChange={onChange}
 			type={inputType}
 			value={inputValue}
